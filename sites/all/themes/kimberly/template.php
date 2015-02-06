d95024bfa0eacb40b9427da1baf4f43e7d34da75
Juan Carlos Orrego <jcorrego@gmail.com>
2015-02-06 09:46:47 -0500

    Foundation Kimberly subtheme

---
+++ sites/all/themes/kimberly/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function kimberly_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function kimberly_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function kimberly_preprocess_node(&$variables) {
+}
