babb030a981772ad81f6c28014c5cfc6fa1f51ed
Juan Carlos Orrego <jcorrego@gmail.com>
2015-03-06 13:58:49 -0500

    User name escaping in custom module

--- sites/all/modules/custom/dsusers/templates/logged_user.tpl.php
+++ sites/all/modules/custom/dsusers/templates/logged_user.tpl.php
@@ -1,10 +1,8 @@
 <div class="row ">
-  <div class="welcome columns small-10 text-right">Bienvenido(a): <span class="name"><?php echo $data["nombre"]?></span>
+  <div class="welcome columns small-10 text-right">Bienvenido(a): <span class="name"><?php echo htmlentities($data["nombre"])?></span>
     <div class="logout"><a href="/user/logout">Cerrar sesi&oacute;n</a></div>
   </div>
   <div class="myaccount columns small-6 text-center"><a href="/user">Mi cuenta</a></div>
 </div>
-
-
 <?php
 //print_r($data);
