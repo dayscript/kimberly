c1611c97f003ccc848e97bc83c3abde385fc31a1
Juan Carlos Orrego <jcorrego@gmail.com>
2015-04-10 11:51:46 -0500

    Correction for displaying image in "presentacion" block

--- sites/all/modules/custom/dsusers/templates/presentation.tpl.php
+++ sites/all/modules/custom/dsusers/templates/presentation.tpl.php
@@ -1,1 +1,3 @@
-<img src="<?php echo image_style_url('escalar_a_341', $data['presentacion']->field_image['und'][0]['filename'])?>" />
+<img src="<?php echo image_style_url('escalar_a_341', $data['presentacion']->field_image['und'][0]['uri'])?>" />
+<?php
+//print_r($data['presentacion']->field_image);
