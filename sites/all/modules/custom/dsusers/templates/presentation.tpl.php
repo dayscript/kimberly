af4e5b07a523e5b5929c2c5cc132248f3f66ce6d
Juan Carlos Orrego <jcorrego@gmail.com>
2015-04-10 12:03:47 -0500

    Link "presentacion" image to PDF

--- sites/all/modules/custom/dsusers/templates/presentation.tpl.php
+++ sites/all/modules/custom/dsusers/templates/presentation.tpl.php
@@ -1,3 +1,5 @@
-<img src="<?php echo image_style_url('escalar_a_341', $data['presentacion']->field_image['und'][0]['uri'])?>" />
+<a target="_blank" href="<?php echo file_create_url($data['presentacion']->field_documento_adjunto['und'][0]['uri'])?>">
+    <img src="<?php echo image_style_url('escalar_a_341', $data['presentacion']->field_image['und'][0]['uri'])?>" />
+</a>
 <?php
-//print_r($data['presentacion']->field_image);
+//print_r($data['presentacion']->field_documento_adjunto);
