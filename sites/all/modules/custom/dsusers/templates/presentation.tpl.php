dc84f8ab93b4631fd4832fef49cbab52e8d9df76
Juan Carlos Orrego <jcorrego@gmail.com>
2015-04-13 16:02:16 -0500

    Presetacion styles updates

--- sites/all/modules/custom/dsusers/templates/presentation.tpl.php
+++ sites/all/modules/custom/dsusers/templates/presentation.tpl.php
@@ -3,21 +3,16 @@
     <img
         src="<?php echo image_style_url( 'escalar_a_341', $data[ 'presentacion' ]->field_image[ 'und' ][ 0 ][ 'uri' ] ) ?>"/>
 </a>
-<?php
-//print_r($data['presentacion']->field_documento_adjunto);
-global $user;
-$user_profile = user_load( $user->uid );
-if ( isset( $user_profile->field_segmento ) && isset( $user_profile->field_segmento[ 'und' ][ 0 ] ) ){
-    $segmento = $user_profile->field_segmento[ 'und' ][ 0 ][ 'tid' ];
-} else{
-    return true;
-}
-if ( isset( $row->field_segmento ) && isset( $row->field_segmento[ 'und' ] ) && is_array( $row->field_segmento[ 'und' ] ) && count( $row->field_segmento[ 'und' ] ) > 0 ){
-    foreach($row->field_segmento[ 'und' ] as $rowsegmento){
-        if($rowsegmento['tid'] == $segmento)
-            return false;
-    }
-    return true;
-} else{
-    return true;
-}
+<div class="views-field views-field-nothing">
+    <span class="field-content">
+        <div class="arrow"><img src="/sites/all/themes/kimberly/images/backgrounds/arrow.png"></div>
+    </span>
+</div>
+<div class="views-field views-field-body">
+    <span class="field-content">
+        <a target="_blank"
+           href="<?php echo file_create_url( $data[ 'presentacion' ]->field_documento_adjunto[ 'und' ][ 0 ][ 'uri' ] ) ?>">
+            <?php print_r( $data[ 'presentacion' ]->body['und'][0]['value'] )?>
+        </a>
+    </span>
+</div>
