6814e6f9f5197b96c8f164c57224fa68ebbf0dfb
aacevedoday <aacevedo@dayscript.com>
2015-09-14 20:43:34 -0500

    update

--- sites/all/themes/kimberly/js/_kimberly.js
+++ sites/all/themes/kimberly/js/_kimberly.js
@@ -1,1 +1,11 @@
 /* Implement customer javascript here */
+
+/* Implement customer javascript here */
+(function($) {
+ 
+ 	$( document ).ready(function() {
+		$('.reveal-modal').foundation('reveal', 'open');
+	});
+
+
+})(jQuery);
